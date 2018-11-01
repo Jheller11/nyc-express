833e60cf8b58c5f88bdbaf78e85358fe17c6db5b
Jeffrey Heller <heller.jeffrey@gmail.com>
2018-11-01 11:09:04 -0400

    add controllers to index

--- index.js
+++ index.js
@@ -2,12 +2,25 @@ const app = require('express')()
 const parser = require('body-parser')
 const cors = require('cors')
 const methodOverride = require('method-override')
+const session = require('express-session')
+const passport = require('passport')
+const itemController = require('./controllers/items')
+const userController = require('./controllers/users')
 
 // config middleware
 app.use(cors())
 app.use(parser.json())
 app.use(parser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+
+// passport
+app.use(session({ secret: 'j' }))
+app.use(passport.initialize())
+app.use(passport.session())
+
+// controllers
+app.use('/items', itemController)
+app.use('/users', userController)
 
 app.get('/', (req, res) => {
   res.send('hello')
